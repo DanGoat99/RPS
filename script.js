bb3e8fd9289db215503941165accba8e33c8cd14
DanGoat99 <166734786+DanGoat99@users.noreply.github.com>
2024-06-11 14:02:47 -0700

    uplo

--- script.js
+++ script.js
@@ -4,23 +4,13 @@ let paper;
 let scissors;
 const h1 = document.createElement("h1");
 let computerSelection;
-let humanScore;
-let computerScore =0;
-let getHumanChoice=0;
+let humanScore = 0;
+let computerScore = 0;
+let getHumanChoice;
 
 // New Game
 const NewGame = document.getElementsByClassName("New-Game");
 NewGame[0].addEventListener("click", gamestart);
-
-function gamestart() {
-  console.log("hello world");
-  computerChoice();
-  console.log(computerSelection);
-  //HUMAN CHOICE
-  getHumanChoice = prompt("Human Selection").toUpperCase();
-  //getHumanChoice = getHumanChoice.toUpperCase;
-  console.log(getHumanChoice);
-}
 
 // COMPUTER CHOICE
 function computerChoice() {
@@ -32,16 +22,53 @@ function computerChoice() {
   } else {
     computerSelection = "PAPER";
   }
+  console.log(`${computerSelection} is computer choice`);
 }
 
 function playRound(computerSelection, getHumanChoice) {
-  if (computerSelection === "ROCK" && getHumanChoice === "SCISSORS"){computerScore++1};
-  else if (computerSelection==="ROCK" && getHumanChoice==="PAPER"){humanScore++1};
-  else if (computerSelection==="PAPER" && getHumanChoice==="ROCK") {}
+  function computerChoice() {
+    let cc = Math.floor(Math.random() * 3) + 1;
+    if (cc === 1) {
+      computerSelection = "ROCK";
+    } else if (cc === 2) {
+      computerSelection = "SCISSORS";
+    } else {
+      computerSelection = "PAPER";
+    }
+    console.log(`${computerSelection} is computer choice`);
+  }
+  computerChoice();
+  getHumanChoice = prompt("Human Slection").toUpperCase();
 
-  else {/*draw playRound*/}
+  if (computerSelection === getHumanChoice) {
+    alert(`Both players chose ${computerSelection} 
+      Round is a draw`);
+    i--;
+    console.log(`Round is ${i}`);
+    console.log(`Computer: ${computerScore}, Human: ${humanScore}`);
+  } else if (
+    (computerSelection === "PAPER" && getHumanChoice === "SCISSORS") ||
+    (computerSelection === "SCISSORS" && getHumanChoice === "ROCK") ||
+    (computerSelection === "ROCK" && getHumanChoice === "PAPER")
+  ) {
+    console.log(`${getHumanChoice} beats ${computerSelection}, Human Wins`);
+    humanScore += 1;
+    console.log(`Computer: ${computerScore}, Human: ${humanScore}`);
+  } else {
+    console.log(`${computerSelection} beats ${getHumanChoice} Computer Wins`);
+    computerScore += 1;
+    console.log(`Computer: ${computerScore}, Human: ${humanScore}`);
+  }
 }
 
+function gamestart() {
+  computerScore = 0;
+  humanScore = 0;
+  for (i = 1; i < 6; i++) {
+    playRound();
+    console.log(`Round is ${i}`);
+  }
+}
 //game round 1/3 (in html shows current round)
 ///computer choice (in html shows computer choice which is a random)
 ///player choice
@@ -49,5 +76,3 @@ function playRound(computerSelection, getHumanChoice) {
 //allocate point to winner
 //start new round
 //first player to 3 points wins
-
-// offer rematch
